c2c540ce056da158480c3fb95f01528b6287b633
Cris Perez <cristino.perez525@gmail.com>
2020-11-02 23:00:38 -0600

    removed code from server.js that didn't belong to this assignment

--- server.js
+++ server.js
@@ -13,21 +13,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.post("/submit", ({ body }, res) => {
-  const user = new User(body);
-  user.coolifier();
-  user.makeCool();
-
-  User.create(user)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
