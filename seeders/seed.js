24aae2fcff237d9596043619c7cddb297852a7e6
Cris Perez <cristino.perez525@gmail.com>
2020-11-05 10:53:27 -0600

    added models folder, added index.js file, updated models.js to schema.js, correctly hooked up seed.js file to model so that it added data appropriately

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -124,8 +124,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+
+  db.Workout.collection.insertMany(workoutSeed)
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
@@ -134,3 +134,8 @@ db.Workout.deleteMany({})
     console.error(err);
     process.exit(1);
   });
+
+  db.Workout.find({}).then(data => {
+
+    console.log(data[0].exercises)
+  })
