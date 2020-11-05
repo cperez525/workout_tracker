28ce391897ee2e489b3a3aede98a0c84b1127543
Cris Perez <cristino.perez525@gmail.com>
2020-11-05 15:43:48 -0600

    Updated server.js and seeds file for heroku deployment

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ app.use(express.static("public"));
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
