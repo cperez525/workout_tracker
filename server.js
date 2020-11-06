764c2e34b16f34d300ca787441a83d814036d76a
Cris Perez <cristino.perez525@gmail.com>
2020-11-05 22:11:29 -0600

    Trying more to fix deployment issue

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-).then(
-   ()=>{ console.log("Connected")
-   .catch((err)=>{
-      console.log("ERROR:", err);
-   }
-);
+     process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+   ).then(
+      ()=>{ console.log("Connected")}
+   ).catch((err)=>{
+         console.log("ERROR:", err);
+      }
+   );
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
